daa81606b96bd099d1fd3a9cef242a7c7e3d483d
neha <neha.angularjs@gmail.com>
2021-04-12 21:15:00 +0530

    updates to deploy to heroku

--- server.ts
+++ server.ts
@@ -13,6 +13,7 @@ import { existsSync } from 'fs';
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/SpaceLaunch/browser');
+  
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
